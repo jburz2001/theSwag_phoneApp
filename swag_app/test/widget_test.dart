2d8582e9c73970ef875fe90f1e32802663ff0471
Justin Burzachiello <justinburzachiello@gmail.com>
2021-12-15 20:11:10 -0800

    made file for original main.dart

--- swag_app/test/widget_test.dart
+++ swag_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:swag_app/main.dart';
+import 'package:swag_app/original_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
