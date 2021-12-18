43dcbacd96b46a8b0e0095ed255b1bc26f550b39
Justin Burzachiello <justinburzachiello@gmail.com>
2021-12-18 06:45:54 -0800

    professional home page

--- swag_app/lib/main.dart
+++ swag_app/lib/main.dart
@@ -6,22 +6,64 @@ void main() => runApp(MaterialApp(
   home: Home(),
 ));
 
-class Home extends StatelessWidget {  
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("SWAG App"),
+        title: Text("SWAG"),
         centerTitle: true,
+        backgroundColor: Colors.blue[200],
       ),
-
-      body: Center(
-        child: Text("Yoo o"),
-      ),
-      
-      floatingActionButton: FloatingActionButton(
-        child: Text("hey"),
-        onPressed: () {},
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.logout),
+                  label: Text("Log out"),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[100],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.people),
+                  label: Text("People"),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[100],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.info),
+                  label: Text("Information"),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[100],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
